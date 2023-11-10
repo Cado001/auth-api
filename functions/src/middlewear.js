3340a0513182bf01d4f13ce48244efd7d14376ef
Micado Pierre <Biml16gb@gmail.com>
2023-11-10 12:24:48 -0500

    auth-api

--- functions/src/middlewear.js
+++ functions/src/middlewear.js
@@ -1,23 +1,24 @@
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { secretKey } from "./creds.js";
 
 
-export async function isAuthenticated(req, res, next){
+export async function isAuthenticated(req, res, next) {
     const { authorization } = req.headers;
     //first check if they HAVE a token:
-    if(!authorization) {
+    if (!authorization) {
         res.send(401).send({ message: 'No authorization token found' })
         return;
     }
     // then check if the token is VALID:
-    jwt.verify = jwt.decode(authorization, secretKey)
-       .then(decoded => { // valid token
-        req.locals = decoded; //attach our decoded token to the request...
-           // If so, go on
-        next();
+    jwt.verify = jwt.decode(authorization, secretKey, (err, decoded) {
+        if (err) {
+            res.status(401).send(err);
+            return;
+        }
 
-       })
-       .catch(err => {  // not valid token 
-        res.status(401).send(err);
-       })
+        // Valid token:
+        req.locals = decoded; // attach out decoded token to the request
+        // if so, go on
+        next();
+    });
 }
